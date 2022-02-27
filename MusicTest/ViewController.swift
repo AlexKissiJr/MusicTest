a71376cef840c43a036c69d12403136bdded43da
Alex Kissi Jr <Alex@createlex.com>
2022-02-26 16:30:45 -0800

    Added Button

--- MusicTest/ViewController.swift
+++ MusicTest/ViewController.swift
@@ -12,8 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        let btn = UIButton()
+        btn.frame = CGRect(x: 0, y: 0, width: 300, height: 500)
+        btn.center = view.center
+        btn.setTitle("Tap", for: .normal)
+        btn.addTarget(self, action: #selector(btnPressed), for: .touchUpInside)
+        view.addSubview(btn)
     }
-
-
 }
 
+extension ViewController {
+    @objc func btnPressed(sender:UIButton){
+        print("Pressed")
+    }
+}
